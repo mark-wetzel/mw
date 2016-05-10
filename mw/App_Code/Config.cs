ab2585f751248b6996b2772a8d938d309082f81e
Mark Wetzel <me@markwetzel.com>
2016-05-09 20:32:50 -0600

    Add page title helper/config class

---
+++ mw/App_Code/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mw.App_Code
+{
+    public static class Config
+    {
+        public const string SiteName = "Mark Wetzel";
+    }
+}
