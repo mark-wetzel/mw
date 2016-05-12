68c3c6bc717097c07f5e19b9e6c5d97d752783fe
Mark Wetzel <me@markwetzel.com>
2016-05-11 19:38:29 -0600

    Make entry datetimes optional

--- mw/Models/Entry.cs
+++ mw/Models/Entry.cs
@@ -10,9 +10,9 @@ namespace mw.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Body { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public string Body { get; set; }        
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 
     public class EntryContext : DbContext
