9ec5c4395abdb1b35838068ff403c2ed20ec7076
Mark Wetzel <me@markwetzel.com>
2016-05-11 19:25:17 -0600

    Add entries

---
+++ mw/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace mw.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<mw.Models.EntryContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "mw.Models.EntryContext";
+        }
+
+        protected override void Seed(mw.Models.EntryContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ mw/Models/Entry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mw.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class EntryContext : DbContext
+    {
+        public DbSet<Entry> Entries { get; set; }
+    }
+}
