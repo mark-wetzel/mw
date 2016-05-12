d7ab3a17032fdc2b90af38682fdedf9a347c6f6e
Mark Wetzel <me@markwetzel.com>
2016-05-11 19:43:36 -0600

    Add home route

--- mw/Models/Entry.cs
+++ mw/Models/Entry.cs
@@ -11,8 +11,8 @@ namespace mw.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 
     public class EntryContext : DbContext
