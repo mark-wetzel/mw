933791aba704fbece4d7c3bd13693c8852560ada
Mark Wetzel <me@markwetzel.com>
2016-05-15 09:41:16 -0600

    Migrations for projects

---
+++ mw/Migrations/ProjectContext/Configuration.cs
@@ -0,0 +1,33 @@
+namespace mw.Migrations.ProjectContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<mw.Models.ProjectContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Migrations\ProjectContext";
+            ContextKey = "mw.Models.ProjectContext";
+        }
+
+        protected override void Seed(mw.Models.ProjectContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
