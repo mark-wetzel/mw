d959e2ed4be319a9a4511384b933ae014debdf40
Mark Wetzel <me@markwetzel.com>
2016-05-21 12:10:29 -0600

    Fix merge confict

--- mw/ViewModels/CreateProjectViewModel.cs
+++ mw/ViewModels/CreateProjectViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mw.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,5 +10,6 @@ namespace mw.ViewModels
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public HttpPostedFileBase Image { get; set; }
     }
 }
