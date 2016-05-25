ddc95f935476bd7d130673bcb546b0463e9c2f6f
Mark Wetzel <me@markwetzel.com>
2016-05-25 16:01:42 -0600

    Add image input to project edit page

--- mw/ViewModels/EditProjectViewModel.cs
+++ mw/ViewModels/EditProjectViewModel.cs
@@ -10,5 +10,6 @@ namespace mw.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public HttpPostedFileBase Image { get; set; }
     }
 }
